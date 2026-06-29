b096603dcee94c6c08538ebf1f7b242c66ac3209
Giovani de Pauli <giovanipauli@gmail.com>
2026-06-29 12:30:50 -0300

    feat: Implementação de operações simples (adição, subtração, multiplicação e divisão).

---
+++ Calculator.Domain/Operations/Multiplication.cs
@@ -0,0 +1,9 @@
+﻿using Calculator.Domain.Interfaces;
+
+namespace Calculator.Domain.Operations
+{
+    public class Multiplication : IOperation
+    {
+        public decimal Execute(decimal left, decimal right) => left * right;
+    }
+}
